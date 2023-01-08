7a3590fda2ac74013ce4f3ad47005646db3f967b
Thiago Santos <t.mateus.santos2004@gmail.com>
2023-01-08 21:38:00 +0000

    Update dashboard.jsx

--- src/escoladigital/pages/user/dashboard.jsx
+++ src/escoladigital/pages/user/dashboard.jsx
@@ -4,6 +4,7 @@ import { useState } from 'react'
 
 const dashboard = () => {
 
+
 //get server side props
  async function getServerSideProps(context) {
     const { req, res } = context;
@@ -37,6 +38,7 @@ const dashboard = () => {
     };
 }
 
+
   return (
 
     <div className="min-h-full min-w-full flex items-center justify-center py-16 px-2 sm:px-6 lg:px-4 ">
@@ -48,40 +50,12 @@ const dashboard = () => {
             <div className="flex items-center">
                 <h2 className="text-xl font-bold">Dashboard</h2>
             </div>
-            <div className="relative">
-                <span className="absolute inset-y-0 left-0 flex items-center py-4">
-                    <button
-                        type="submit"
-                        className="p-2 focus:outline-none focus:ring"
-                    >
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            className="w-6 h-6"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                            strokeWidth={2}
-                        >
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                            />
-                        </svg>
-                    </button>
-                </span>
-                <input
-                    type="search"
-                    name="Search"
-                    placeholder="Search..."
-                    className="w-full py-2 pl-10 text-sm rounded-md focus:outline-none"
-                />
-            </div>
+
             <div className="flex-1">
                 <ul className="pt-2 pb-4 space-y-1 text-sm">
                     <li className="rounded-sm">
                         <a
-                            href="#"
+                            href="/"
                             className="flex items-center p-2 space-x-3 rounded-md"
                         >
                             <svg
@@ -103,7 +77,7 @@ const dashboard = () => {
                     </li>
                     <li className="rounded-sm">
                         <a
-                            href="#"
+                            href=""
                             className="flex items-center p-2 space-x-3 rounded-md"
                         >
                             <svg
@@ -187,22 +161,8 @@ const dashboard = () => {
                     12,00
                 </div>
             </div>
-            <div className="w-full px-4 py-5 bg-white rounded-lg shadow">
-                <div className="text-sm font-medium text-gray-500 truncate">
-                    Total Profit
-                </div>
-                <div className="mt-1 text-3xl font-semibold text-gray-900">
-                    $ 450k
-                </div>
-            </div>
-            <div className="w-full px-4 py-5 bg-white rounded-lg shadow">
-                <div className="text-sm font-medium text-gray-500 truncate">
-                    Total Orders
-                </div>
-                <div className="mt-1 text-3xl font-semibold text-gray-900">
-                    20k
-                </div>
-            </div>
+
+
         </div>
     </div>
     </div>
