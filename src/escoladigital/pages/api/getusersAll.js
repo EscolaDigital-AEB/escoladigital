cc783ed889bc0d62127b2bfddaf9b44e2296aaa3
Thiago Santos <t.mateus.santos2004@gmail.com>
2023-01-09 11:52:17 +0000

    Update getusersAll.js

--- src/escoladigital/pages/api/getusersAll.js
+++ src/escoladigital/pages/api/getusersAll.js
@@ -11,44 +11,7 @@ export default async function handler(req, res) {
 
 
 
-      //get all names
-      let nomes = [];
-      for (let i = 0; i < response.length; i++) {
-        nomes.push(response[i].nome);
-      }
-
-      //get all emails
-      let emails = [];
-      for (let i = 0; i < response.length; i++) {
-        emails.push(response[i].email);
-      }
-
-
-      //get all roles
-      let roles = [];
-      for (let i = 0; i < response.length; i++) {
-        roles.push(response[i].role);
-      }
-
-      //get all status
-      let status = [];
-      for (let i = 0; i < response.length; i++) {
-        status.push(response[i].status);
-      }
-
-      //get all created_at
-      let created_at = [];
-      for (let i = 0; i < response.length; i++) {
-        created_at.push(response[i].created_at);
-      }
-
-      //get all updated_at
-      let updated_at = [];
-      for (let i = 0; i < response.length; i++) {
-        updated_at.push(response[i].updated_at);
-      }
-
-  
+     
 
       let all = {
         all: response,
